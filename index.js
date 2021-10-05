15d7961eeda8af5b8dc4c482d94d4ccee5801246
nicmlu <NicoleMLucien@gmail.com>
2021-10-05 13:50:56 -0400

    commit tree ddc69ca14c6c759a0ed1d6c2636516e1edf4da75

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-
 // Write your code here
+
+let num1 = 2;
+let num2 = 31;
+
+let multiply = num1 * num2;
+multiply;
+
+let random = Math.floor(Math.random() * 10) + 1;
+random;
+
+let num3 = 64;
+let num4 = 10;
+
+let mod = num3 % num4;
+mod;
+
+let max = Math.max(2, 3, 6, 13, 20);
+max;
